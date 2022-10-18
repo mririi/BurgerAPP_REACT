5d722788b8a7ea709c3c946efd92ee5dbaf54f6f
mririi <wassim.mriri@gmail.com>
2022-10-18 20:43:50 +0100

    Finished sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import AuxComponent from '../../hoc/AuxComponent'
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import "./Layout.css"
-const Layout = (props) => {
-  return (
-    <AuxComponent>
-    <Toolbar />
-    <SideDrawer />
-    <main className="Content">{props.children}</main>
-    </AuxComponent>
-  )
+import React, { Component } from "react";
+import AuxComponent from "../../hoc/AuxComponent";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import "./Layout.css";
+class Layout extends Component {
+  state={
+    showSideDrawer:true
+  }
+  sideDrawerClosedHandler=()=>{
+    this.setState({showSideDrawer:false})
+  }
+  sideDrawerToggleHandler=()=>{
+    this.setState((prevState)=>{return{showSideDrawer:!prevState.showSideDrawer}})
+  }
+  render() {
+    return (
+      <AuxComponent>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className="Content">{this.props.children}</main>
+      </AuxComponent>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,15 +1,20 @@
 import React from "react";
+import AuxComponent from "../../../hoc/AuxComponent";
 import Logo from "../../Logo/Logo";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import "./SideDrawer.css";
 const SideDrawer = (props) => {
   return (
-    <div className="SideDrawer">
-      <Logo height="11%"/>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <AuxComponent>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={props.open ? "Open" : "Closed"}>
+        <Logo height="11%" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </AuxComponent>
   );
 };
 
